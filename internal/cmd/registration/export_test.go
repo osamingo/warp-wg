1fb32204b6e9a61de31b0a826bef65624cb80596
osamingo <1390409+osamingo@users.noreply.github.com>
2026-03-30 16:50:37 +0900

    style(internal/cmd/registration): fix alignment in export_test.go

--- internal/cmd/registration/export_test.go
+++ internal/cmd/registration/export_test.go
@@ -6,7 +6,7 @@ var (
 	ConfirmDelete     = confirmDelete
 	ConfirmRotateKeys = confirmRotateKeys
 	SystemLocale      = systemLocale
-	Print = printRegistration
+	Print             = printRegistration
 	ExecShow          = execShow
 	ExecDevices       = execDevices
 )
